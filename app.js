4e3ae4a6c47df52b6f05ab7be96098efbba45f3b
B1NER0 <degenaarp@gmail.com>
2020-09-18 13:38:41 +0200

    again the port

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const app = express();
 
+const PORT = process.env.PORT || 8080;
+
 app.get('/', (req, res) => {
     res.send("This is the freaking homepage blabla");
 })
 
-app.listen(3000, () => {
+app.listen(PORT, () => {
         console.log("WAZAAAP");
 });
